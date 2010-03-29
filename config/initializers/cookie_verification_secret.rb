a02b8dc32fa12e44c2d1254a723f8c33fe23cba6
Adam Roth <adamjroth@gmail.com>
2010-03-29 14:54:12 -0400

    initial commit; using rails 3.0.0.beta. Existing code + thoughts exist in the brewday_old repo.

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'a1b45c0146e77ecfa71115aeb4aa87dd79fc2004ca66c5b9e950e2a62bccb81279b9382ff87c7fa400af828efe52cf3faae743464484dc19cd5f003b0fba5f2f'
